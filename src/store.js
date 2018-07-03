8c946e5144d52020ab2e19cb07e64ac400e5b7bb
Amit Feldman <amit@feldman.cl>
2018-07-03 20:21:22 +0300

    add mutations, actions and getters to Store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,29 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    articles: [],
   },
   mutations: {
-
+    FETCH_ARTICLES(state, articles) {
+      state.articles.push(articles);
+    },
   },
   actions: {
+    fetchArticles({ commit }) {
+      const url = 'https://newsapi.org/v2/everything?sources=ynet';
+      const key = 'e3ef295dbf2745cab9ddac02f648444d';
 
+      fetch(`${url}&apiKey=${key}`)
+        .then(res => res.json())
+        .then((payload) => {
+          commit('FETCH_ARTICLES', payload.articles);
+        })
+        .catch((err) => {
+          if (err) throw err;
+        });
+    },
+  },
+  getters: {
+    loadedArticles: state => state.articles,
   },
 });
